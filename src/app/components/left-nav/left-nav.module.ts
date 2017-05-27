747a39159615e4d360118ee40b690138f4c7f677
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-27 23:36:59 +0700

    Init LeftNav component

---
+++ src/app/components/left-nav/left-nav.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LeftNavComponent } from './left-nav.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LeftNavComponent]
+})
+export class LeftNavModule { }
