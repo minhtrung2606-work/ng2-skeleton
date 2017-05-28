4067d93478a60bc6426d551eec1c014bfe06b1f9
Trung (Minh) Nguyen <nminhtrung@tma.com.vn>
2017-05-28 13:43:09 +0700

    Add model used for LeftNavMenuItem component

---
+++ src/app/components/left-nav/left-nav-menu-item/model/left-nav-menu-item.ts
@@ -0,0 +1,80 @@
+export class LeftNavMenuItem {
+  private primary: boolean;
+  private statistic: boolean;
+  private startIcon: string;
+  private label: string;
+  private endIcon: string;
+  private count: number;
+
+  constructor(label: string, isPrimary = true, statistic = false, startIcon?: string, endIcon?: string, count = 0) {
+    this.primary = isPrimary;
+    this.statistic = statistic;
+    this.startIcon = startIcon;
+    this.label = label;
+    this.endIcon = endIcon;
+    this.count = count;
+  }
+
+  getLabel(): string {
+    return this.label;
+  }
+
+  setLabel(label: string): LeftNavMenuItem {
+    this.label = label;
+    return this;
+  }
+
+  isPrimary(): boolean {
+    return this.primary;
+  }
+
+  setPrimary(primary: boolean): LeftNavMenuItem {
+    this.primary = primary;
+    return this;
+  }
+
+  isStatistic(): boolean {
+    return this.statistic;
+  }
+
+  setStatistic(statistic: boolean): LeftNavMenuItem {
+    this.statistic = statistic;
+    return this;
+  }
+
+  getStartIcon(): string {
+    return this.startIcon;
+  }
+
+  setStartIcon(startIcon: string): LeftNavMenuItem {
+    this.startIcon = startIcon;
+    return this;
+  }
+
+  getLabel(): string {
+    return this.label;
+  }
+
+  setLabel(label: string): LeftNavMenuItem {
+    this.label = label;
+    return this;
+  }
+
+  getEndIcon(): string {
+    return this.endIcon;
+  }
+
+  setEndIon(endIcon: string): LeftNavMenuItem {
+    this.endIcon = endIcon;
+    return this;
+  }
+
+  getCount(): number {
+    return this.count;
+  }
+
+  setCount(count: number): LeftNavMenuItem {
+    this.count = count;
+    return this;
+  }
+}
