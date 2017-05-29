6b13acf3f1bf5ec821b1d1863bfca11d262f0bbe
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-29 20:22:19 +0700

    Update LeftNav component template. Build data for LeftNav component in root app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  LeftNavMenuItem
+} from './components/left-nav/left-nav-menu-item/model/left-nav-menu-item';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +20,26 @@ export class AppComponent {
 
 function buildLeftNavItemList(productCategory) {
   if (productCategory) {
-    
+    return _.map(productCategory, function (productCat) {
+      var menuItem = new LeftNavMenuItem(productCat.label);
+      menuItem
+        .setStartIcon(productCat.icon)
+        .setStatistic(true)
+        .setCount(productCat.count)
+      ;
+
+      _.each(productCat.itemList, function (prod) {
+        let subMenuItem = new LeftNavMenuItem(prod.label);
+        subMenuItem
+          .setStatistic(true)
+          .setCount(prod.count)
+        ;
+
+        menuItem.addSubMenuItem(subMenuItem);
+      });
+
+      return menuItem;
+    })
   } else {
     return [];
   }
