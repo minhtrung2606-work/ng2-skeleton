8420f71bcaa4068a159fb6ba6efa97b4a5347989
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-30 20:14:03 +0700

    Merge source code from LeftNavMenuItem component to SideBarMenuItem component

--- src/app/components/side-bar/side-bar-menu-item/model/side-bar-menu-item.ts
+++ src/app/components/side-bar/side-bar-menu-item/model/side-bar-menu-item.ts
@@ -1,2 +1,63 @@
 export class SideBarMenuItem {
+  private active: boolean;
+  private label: string;
+  private icon: string;
+  private count: number;
+  private subMenuItemList: Array<SideBarMenuItem>;
+
+  constructor(label: string, icon?: string, count?: number) {
+    this.label = label;
+    this.icon = icon;
+    this.count = count;
+    this.subMenuItemList = [];
+  }
+
+  isActive(): boolean {
+    return this.active;
+  }
+
+  setActive(active: boolean): SideBarMenuItem {
+    this.active = active;
+    return this;
+  }
+
+  getLabel(): string {
+    return this.label;
+  }
+
+  setLabel(label: string): SideBarMenuItem {
+    this.label = label;
+    return this;
+  }
+
+  getIcon(): string {
+    return this.icon;
+  }
+
+  setIcon(icon: string): SideBarMenuItem {
+    this.icon = icon;
+    return this;
+  }
+
+  isStatistic(): boolean {
+    return !isNaN(this.count);
+  }
+
+  getCount(): number {
+    return this.count;
+  }
+
+  setCount(count: number): SideBarMenuItem {
+    this.count = count;
+    return this;
+  }
+
+  getSubMenuItemList(): Array<SideBarMenuItem> {
+    return this.subMenuItemList;
+  }
+
+  addSubMenuItem(subMenuItem: SideBarMenuItem): SideBarMenuItem {
+    this.subMenuItemList.push(subMenuItem);
+    return this;
+  }
 }
--- src/app/components/side-bar/side-bar-menu-item/side-bar-menu-item.component.ts
+++ src/app/components/side-bar/side-bar-menu-item/side-bar-menu-item.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SideBarMenuItem } from './model/side-bar-menu-item';
 
 @Component({
-  selector: 'app-side-bar-menu-item',
+  selector: 'side-bar-menu-item',
   templateUrl: './side-bar-menu-item.component.html',
   styleUrls: ['./side-bar-menu-item.component.css']
 })
 export class SideBarMenuItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() menuItem: SideBarMenuItem;
+  @Input() activeIcon: string;
+  @Input() inActiveIcon: string;
+  @Input() subMenuItemIcon: string;
+  @Input() collapsed: boolean;
+
+  @Output() onMenuItemClicked: EventEmitter<SideBarMenuItem>;
+  @Output() onSubMenuItemClicked: EventEmitter<SideBarMenuItem>;
+  @Output() onMenuItemIconClicked: EventEmitter<SideBarMenuItem>;
+
+  constructor() {
+    this.onMenuItemClicked = new EventEmitter();
+    this.onSubMenuItemClicked = new EventEmitter();
+    this.onMenuItemIconClicked = new EventEmitter();
+  }
 
   ngOnInit() {
+  }
+
+  onPrimaryItemClicked(menuItem: SideBarMenuItem): void {
+    this.onMenuItemClicked.emit(menuItem);
+  }
+
+  onSecondaryItemClicked(subMenuItem: SideBarMenuItem): void {
+    this.onSubMenuItemClicked.emit(subMenuItem);
+  }
+
+  getActiveIcon(): string {
+    if (this.menuItem.isActive()) {
+      return this.activeIcon;
+    } else {
+      return this.inActiveIcon;
+    }
+  }
+
+  onPrimaryItemIconClicked(menuItem: SideBarMenuItem): void {
+    this.onMenuItemIconClicked.emit(menuItem);
   }
 
 }
