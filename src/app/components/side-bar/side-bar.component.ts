7795fc08283593a65f8539cf103e77fffe7e682c
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-30 20:21:24 +0700

    Merge source code from LeftNav component to SideBar component

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SideBarMenuItem } from './side-bar-menu-item/model/side-bar-menu-item';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +8,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  private currentMenuItem: SideBarMenuItem;
+  private currentSubMenuItem: SideBarMenuItem;
+
+  private collapsed: boolean;
+  private headerMenuItem: SideBarMenuItem;
+
+  @Input() activeIcon: string;
+  @Input() inActiveIcon: string;
+  @Input() subMenuItemIcon: string;
+  @Input() menuItemList: Array<SideBarMenuItem>;
+  @Output() onMenuItemClicked: EventEmitter<SideBarMenuItem>;
+
+  constructor() {
+    this.onMenuItemClicked = new EventEmitter<SideBarMenuItem>();
+    this.headerMenuItem = new SideBarMenuItem('Management Tool')
+      .setIcon('fa-bars').setActive(true);
+  }
 
   ngOnInit() {
+    if (!this.menuItemList) {
+      this.menuItemList = [];
+    }
+  }
+
+  onMenuItemClicked(menuItem: SideBarMenuItem): void {
+    if (this.collapsed) {
+      // Do nothing when collapsed
+      this.onMenuItemClicked.emit(menuItem);
+      return;
+    }
+
+    if (menuItem.isActive()) {
+      // Activated menu item is clicked again and de-activate it
+      menuItem
+        .setActive(false)
+      ;
+      return;
+    }
+
+    if (this.currentMenuItem) {
+      // Remove current active menu item before activating another one
+      this.currentMenuItem
+        .setActive(false)
+      ;
+    }
+
+    // Activate passed in menu item
+    this.currentMenuItem =  menuItem;
+    this.currentMenuItem
+      .setActive(true)
+    ;
+  }
+
+  onSubMenuItemClicked(subMenuItem: SideBarMenuItem): void {
+    if (this.currentSubMenuItem) {
+      // Remove current active sub menu item before activating another one
+      this.currentSubMenuItem
+        .setActive(false)
+      ;
+    }
+
+    // Activate passed in sub menu item
+    this.currentSubMenuItem =  subMenuItem;
+    this.currentSubMenuItem
+      .setActive(true)
+    ;
+  }
+
+  onMenuItemIconClicked(): void {
+    this.collapsed = !this.collapsed;
   }
 
 }
